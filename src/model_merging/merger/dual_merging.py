2032202a88e2908bae7d52986fb9bbf3dd0d4e62
NiccoloSicilian <niccolo.siciliano@gmail.com>
2026-01-20 13:13:55 +0100

    Update dual_merging.py

--- src/model_merging/merger/dual_merging.py
+++ src/model_merging/merger/dual_merging.py
@@ -500,11 +500,14 @@ class DualCommonTaskSpecificMerger(TaskVectorBasedMerger):
 
         for dataset in task_dicts:
             for key in ref_task_dict:
-                
-                u = svd_dicts[dataset][key]["u"].to(self.device)
-                s = svd_dicts[dataset][key]["s"].to(self.device)
-                v = svd_dicts[dataset][key]["v"].to(self.device)
-                task_dicts[dataset][key] = u @ torch.diag_embed(s) @ v
+                is_matrix = "u" in svd_dict[dataset][key]
+                if is matrix:
+                    u = svd_dicts[dataset][key]["u"].to(self.device)
+                    s = svd_dicts[dataset][key]["s"].to(self.device)
+                    v = svd_dicts[dataset][key]["v"].to(self.device)
+                    task_dicts[dataset][key] = u @ torch.diag_embed(s) @ v
+                else:
+                    task_dicts[dataset][key] = svd_dicts[dataset][key]["dim1"].to(self.device)
                 
         pylogger.info("Computing SVD...")
         for key in ref_task_dict:
