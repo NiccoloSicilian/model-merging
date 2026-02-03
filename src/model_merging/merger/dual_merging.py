2551a50236c93b1c0c281d51c5c049c4284abbcd
NiccoloSicilian <niccolo.siciliano@gmail.com>
2026-02-03 17:14:45 +0100

    Update dual_merging.py uniform_mass

--- src/model_merging/merger/dual_merging.py
+++ src/model_merging/merger/dual_merging.py
@@ -167,16 +167,16 @@ def linear_mass_scheduler_per_transfblock(layer_names): #Asuming layers list ord
         if any(skip in name for skip in ['bias', 'ln_', 'class_embedding', 'logit_scale']):
             continue
         if 'visual.conv1.weight' in name or( 'visual.proj' in name and 'out_proj' not in name) or 'visual.positional_embedding' in name:
-            masses[name] =linear_mass(tot_layers,l)
+            masses[name] =uniform_mass(tot_layers,l)
             l += 1
         elif 'visual.transformer.resblocks' in name and 'weight' in name:
             if 'attn.in_proj_weight' in name or 'attn.out_proj.weight' in name or 'mlp.c_fc.weight' in name or 'mlp.c_proj.weight' in name:
                 if name.split('resblocks.')[1].split('.')[0] == block_id: 
-                    masses[name] = linear_mass(tot_layers, l)
+                    masses[name] = uniform_mass(tot_layers, l)
                     l += 1 
                 else:
                     block_id = name.split('resblocks.')[1].split('.')[0]
-                    masses[name] = linear_mass(tot_layers, l)
+                    masses[name] = uniform_mass(tot_layers, l)
                     l += 1
     return masses
     
