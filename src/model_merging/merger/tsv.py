2583747e5b7afbb3197743370de79d20db3c3c58
NiccoloSicilian <niccolo.siciliano@gmail.com>
2026-02-19 16:51:37 +0100

    Update tsv.py

--- src/model_merging/merger/tsv.py
+++ src/model_merging/merger/tsv.py
@@ -68,7 +68,6 @@ class TaskSingularVectorsMerger(TaskVectorBasedMerger):
         merged_encoder = apply_dict_to_model(
             multi_task_vector,
             merged_encoder,
-            coefficient=coefficient,
         )
 
         return merged_encoder
