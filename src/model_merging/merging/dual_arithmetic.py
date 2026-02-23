6fe450c03bafa9213f217f57535ff974fa9593a4
NiccoloSicilian <niccolo.siciliano@gmail.com>
2026-02-23 18:57:19 +0100

    Update dual_arithmetic.py

--- src/model_merging/merging/dual_arithmetic.py
+++ src/model_merging/merging/dual_arithmetic.py
@@ -94,35 +94,52 @@ class Conv2DSVD(Atom):
             for j in range(k):
                 result[:, :, i, j] = svd_orthogonalize(weight[:, :, i, j])
         return result
+def linear_mass_schedule(current_l, tot_layer):
+    return 0.01 + current_l/tot_layer * 0.5    
 
-
-def ViT_B_16(num_classes=512, num_blocks=12, d_embed=768, num_heads=12, patch_size=16, input_channels=3):
+def ViT_B_16(num_classes=512, num_blocks=12, d_embed=768, num_heads=12, patch_size=16, input_channels=3, mass_schedule='uniform'):
     mlp_width = 4 * d_embed
     patch_dim = input_channels * (patch_size ** 2)
-
+    tot_layers= 4*num_blocks+3
     # 1. Patch Embed (conv1 in checkpoint)
     # Note: Checkpoint shows [768, 3, 16, 16] which is a Conv layer
     
     conv1 = Conv2DSVD(fanin=input_channels, fanout=d_embed,kernel_size=patch_size)
+    conv1.mass = linear_mass_schedule(0,tot_layers)
     # 2. Positional & Class Embedding
     visual_pos_embed = LinearSVD(197, d_embed)
+    visual_pos_embed.mass = linear_mass_schedule(1,tot_layers)
+    
+    transformer = None
     # Pre-transformer norm (ln_pre)
-
-    # 3. Transformer Blocks
-    a1 = LinearSVD(d_embed, d_embed) 
-    a2 = LinearSVD(3*d_embed, d_embed) 
-    att = a1@ a2
-
-    m1 = LinearSVD(d_embed, mlp_width)
-    m2 = LinearSVD(mlp_width, d_embed)
-    mlp = m1@ GeLU() @ m2
+    for b in range(num_blocks):
+        # 3. Transformer Blocks
+        a1 = LinearSVD(d_embed, d_embed) 
+        a1.mass = linear_mass_schedule(b*4+2,tot_layers)
+        
+        a2 = LinearSVD(3*d_embed, d_embed) 
+        a2.mass = linear_mass_schedule(b*4+3,tot_layers)
+        
+        att = a1@ a2
     
-    # Residual paths
-    
-    transformer = (mlp @ att) ** num_blocks
+        m1 = LinearSVD(d_embed, mlp_width)
+        m1.mass = linear_mass_schedule(b*4+4,tot_layers)
+        
+        m2 = LinearSVD(mlp_width, d_embed)
+        m2.mass = linear_mass_schedule(b*4+5,tot_layers)
+        
+        mlp = m2@ GeLU() @ m1
+        
+        # Residual paths
+        if transformer:
+            transformer = (mlp @ att) @ transformer
+        else:
+            transformer = (mlp @ att)
 
     # 4. Final Head (ln_post and proj)
     proj = LinearSVD(d_embed, num_classes)
+    proj.mass = linear_mass_schedule(tot_layers,tot_layers)
+    
     # Correct Flow: Input -> Patch -> Pos -> ln_pre -> Transformer -> ln_post -> Head
     return proj @ transformer  @ visual_pos_embed @ conv1
 ###
