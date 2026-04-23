0b4b2a32cfc83d6ac90f503e6a8c27797955e1f2
Niccolo <siciliano.1958541@studenti.uniroma1.it>
2026-04-23 18:28:39 +0200

    alpha paramter to tsv config

--- src/model_merging/merger/tsv.py
+++ src/model_merging/merger/tsv.py
@@ -32,14 +32,14 @@ pylogger = logging.getLogger(__name__)
 
 class TaskSingularVectorsMerger(TaskVectorBasedMerger):
 
-    def __init__(self, svd_path, svd_compress_factor, non_matrix_params_aggregation):
+    def __init__(self, optimal_alpha, svd_path, svd_compress_factor, non_matrix_params_aggregation):
         super().__init__()
 
+        self.optimal_alpha = optimal_alpha
         self.svd_path = svd_path
         self.svd_compress_factor = svd_compress_factor
         self.non_matrix_params_aggregation = non_matrix_params_aggregation
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print(f"🚀 DualMerger initialized on device: {self.device}")
     
     def merge(self, base_model, finetuned_models):
 
@@ -71,6 +71,7 @@ class TaskSingularVectorsMerger(TaskVectorBasedMerger):
         merged_encoder = apply_dict_to_model(
             multi_task_vector,
             merged_encoder,
+            coefficient=self.optimal_alpha,
         )
 
         return merged_encoder
